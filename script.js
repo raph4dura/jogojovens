7a7e4573c236b3b2bde74dff5a81d4d276720419
raph4dura <117762078+raph4dura@users.noreply.github.com>
2026-06-10 17:52:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv8m3MdhaBTLKht62ek85Gb6l2TwlOt2w",
+  authDomain: "quiz-c8ddf.firebaseapp.com",
+  projectId: "quiz-c8ddf",
+  storageBucket: "quiz-c8ddf.firebasestorage.app",
+  messagingSenderId: "308135121789",
+  appId: "1:308135121789:web:5e6b75c6e3ae9c40343214",
+  measurementId: "G-LM23Q9Q6LG"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase conectado!");
 
 const perguntas = [
     {
